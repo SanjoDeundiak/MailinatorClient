a00d67bcd9ff57b74021ae143a872e8061272375
Oleksandr Deundiak <Deundiak@gmail.com>
2017-05-22 12:48:51 +0300

    Initial commit

---
+++ Mailinator/MailinatorClient.h
@@ -0,0 +1,20 @@
+//
+//  Mailinator.h
+//  Mailinator
+//
+//  Created by Oleksandr Deundiak on 5/22/17.
+//  Copyright © 2017 Virgil Security. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mailinator.
+FOUNDATION_EXPORT double MailinatorVersionNumber;
+
+//! Project version string for Mailinator.
+FOUNDATION_EXPORT const unsigned char MailinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mailinator/PublicHeader.h>
+
+#import "Mailiantor.h"
+#import "MModelsPublic.h"
---
+++ Source/Mailinator/Models/MModelsPublic.h
@@ -0,0 +1,12 @@
+//
+//  MModelsPublic.h
+//  Mailinator
+//
+//  Created by Oleksandr Deundiak on 5/22/17.
+//  Copyright © 2017 Virgil Security. All rights reserved.
+//
+
+#import "MEmail.h"
+#import "MEmailMetadata.h"
+#import "MEmailResponse.h"
+#import "MPart.h"
